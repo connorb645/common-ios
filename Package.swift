23b1c81dc6847e790e0ff68041bd5ad096b5fcaf
Connor Black <connor.b645@gmail.com>
2025-03-28 17:06:01 +0000

    Changed package name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "common-ios",
+    name: "Common",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "common-ios",
-            targets: ["common-ios"]),
+            name: "Common",
+            targets: ["Common"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "common-ios"),
+            name: "Common"),
 
     ]
 )
