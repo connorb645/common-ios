4079fedcf35a7c6f33a3be104cbddd7183dcd96d
Connor Black <connor.b645@gmail.com>
2025-03-31 14:57:57 +0100

    Adds PlayerView

---
+++ Sources/Features/PlayerView.swift
@@ -0,0 +1,51 @@
+//
+//  PlayerView.swift
+//  Common
+//
+//  Created by Connor Black on 31/03/2025.
+//
+
+import Foundation
+import SwiftUI
+import AVFoundation
+import UIKit
+
+public struct PlayerView: UIViewRepresentable {
+    public func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    public func makeUIView(context: Context) -> UIView {
+        return LoopingPlayerUIView(frame: .zero)
+    }
+}
+
+final public class LoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        // TODO: - Make url injectable
+        let fileUrl = Bundle.main.url(forResource: "workout-onboarding", withExtension: "mp4")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+        // Setup the player
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        // Start the movie
+        player.play()
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
